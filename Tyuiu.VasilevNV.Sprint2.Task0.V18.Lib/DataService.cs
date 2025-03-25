39741b734ae1109511e5a9902f925485bbddf897
Николай Васильев <nvasiljev1977@gmail.com>
2025-03-25 18:44:46 +0400

    Add project files.

---
+++ Tyuiu.VasilevNV.Sprint2.Task0.V18.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.VasilevNV.Sprint2.Task0.V18.Lib
+{
+
+    public class DataService : ISprint2Task0V18
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        { 
+            bool[] res = new bool[6];
+            res[0] = x  == y;
+            res[1] = x != y;
+            res[2] = (x - 4701) < y;
+            res[3] = x > (y + 4630);
+            res[4] = (x - 4701) <= y;
+            res[5] = x >= (y * 11);
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.VasilevNV.Sprint2.Task0.V18.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+using Tyuiu.VasilevNV.Sprint2.Task0.V18.Lib;
+namespace Tyuiu.VasilevNV.Sprint2.Task0.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 5175;
+            int y = 475;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            bool[] wait = new bool[6] { false, true, true, true, true, false };
+
+            CollectionAssert.AreEqual(wait, res);
+
+        }
+    }
+}
